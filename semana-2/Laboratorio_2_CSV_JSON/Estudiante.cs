662603bf2651f132580c723213c64b4921a3bec4
Camila <crmarcos@correo.url.edu.gt>
2026-07-20 08:30:24 -0600

    lab2

---
+++ semana-2/Laboratorio_2_CSV_JSON/Estudiante.cs
@@ -0,0 +1,6 @@
+public class Esudiante
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Carrera { get; set; }
+}
---
+++ semana-2/Laboratorio_2_CSV_JSON/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace Laboratorio_2_CSV_JSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ruta = "estudiantes.csv";
+            string[] lineas = File.ReadAllLines(ruta);
+
+            List<Esudiante> estudiantes = new List<Esudiante>();
+
+            for (int i = 1; i < lineas.Length; i++)
+            {
+                string[] datos = lineas[i].Split(',');
+
+                Esudiante estudiante = new Esudiante
+                {
+                    Id = int.Parse(datos[0]),
+                    Nombre = datos[1],
+                    Carrera = datos[2]
+                };
+
+                estudiantes.Add(estudiante);
+            }
+
+            foreach (var estudiante in estudiantes)
+            {
+                Console.WriteLine($"{estudiante.Id} - {estudiante.Nombre} - {estudiante.Carrera}");
+            }
+
+            var opciones = new JsonSerializerOptions { WriteIndented = true };
+            string json = JsonSerializer.Serialize(estudiantes, opciones);
+
+            File.WriteAllText("estudiantes.json", json);
+        }
+    }
+}
