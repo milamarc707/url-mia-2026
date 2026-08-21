d6b117b6f0ef2931252bf2171223f5634ae82523
Camila <crmarcos@correo.url.edu.gt>
2026-08-21 09:37:18 -0600

    semana 2

--- semana-2/Laboratorio_2_CSV_JSON/obj/Debug/net10.0/Laboratorio_2_CSV_JSON.AssemblyInfo.cs
+++ semana-2/Laboratorio_2_CSV_JSON/obj/Debug/net10.0/Laboratorio_2_CSV_JSON.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio_2_CSV_JSON")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0737fee5e381e3d7c9b9e85a6b8e8930b5a0be0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b1e0c5e0d7bda13a8b5139a1902001bc3b78e93")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laboratorio_2_CSV_JSON")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio_2_CSV_JSON")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
